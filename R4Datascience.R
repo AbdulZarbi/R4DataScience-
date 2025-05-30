9bb3f12b2cc01c05c6d7e518c7ddfca275e1f1b1
AWZ <57395474+AbdulZarbi@users.noreply.github.com>
2025-05-29 22:52:09 -0500

    ggplot

--- R4Datascience.R
+++ R4Datascience.R
@@ -39,9 +39,23 @@ ggplot(
       data = penguins,
       mapping = aes(x = flipper_length_mm, y = body_mass_g, color = species)
 ) +
-      geom_point()
-#> Warning: Removed 2 rows containing missing values or values outside the scale range
-#> (`geom_point()`).
+      geom_point(mapping = aes(color = species)
+                 ) +
+      geom_smooth(method = "lm")
+
+ggplot(
+      data = penguins,
+      mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+      geom_point(mapping = aes(color = species, shape = species)) +
+      geom_smooth(method = "lm") +
+      labs (
+            title = "Body mass and flipper lenght",
+            subtitle = " Dimension for Adelie, Chinstrap, and gentoo penguins",
+            x = "Flipper length (mm)", y = "Body mass (g)",
+            color = "Species", shape = "Species"
+      ) +
+      scale_color_colorblind()
 
 
 
