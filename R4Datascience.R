1280cd9d855b7cd204df69c8f80abaf9d84dd1e9
AWZ <57395474+AbdulZarbi@users.noreply.github.com>
2025-05-29 22:52:48 -0500

    ggplot

--- R4Datascience.R
+++ R4Datascience.R
@@ -14,6 +14,7 @@ install.packages(
 )
 
 1 + 2
+# ggplot2 implements the grammar of graphics
 
 install.packages("palmerpenguins")
 
@@ -56,6 +57,109 @@ ggplot(
             color = "Species", shape = "Species"
       ) +
       scale_color_colorblind()
+#How many rows are in penguins? How many columns?
+nrow(penguins)
+
+?penguins
+#Make scarter plot of bill depth and bill length?
+
+ggplot(
+      data = penguins, 
+      mapping = aes(x = bill_length_mm, y = bill_depth_mm)
+) +
+      geom_point(mapping = aes(color = species, shape = species))
+
+ggplot(data = penguins) + 
+      geom_point()
+
+ggplot(
+      data = penguins,
+      mapping = aes(x = flipper_length_mm, y = body_mass_g, color = island)
+) +
+      geom_point() +
+      geom_smooth(se = FALSE)
 
 
+ggplot(
+      data = penguins,
+      mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+      geom_point() +
+      geom_smooth()
 
+ggplot() +
+      geom_point(
+            data = penguins,
+            mapping = aes(x = flipper_length_mm, y = body_mass_g)
+      ) +
+      geom_smooth(
+            data = penguins,
+            mapping = aes(x = flipper_length_mm, y = body_mass_g)
+      )
+
+#ggplot2 calls
+ggplot(
+      data = penguins,
+      mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+      geom_point()
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) + 
+      geom_point()
+#pipe
+penguins |> 
+      ggplot(aes(x = flipper_length_mm, y = body_mass_g)) + 
+      geom_point()
+
+
+#Visualizing distributions
+
+ggplot(penguins, aes(x = species)) +
+      geom_bar()
+
+ggplot(penguins, aes(x = fct_infreq(species))) +
+      geom_bar()
+
+#A numerical variable 
+
+ggplot(penguins, aes(x = body_mass_g)) + 
+      geom_histogram(binwidth = 200)
+
+
+ggplot(penguins, aes(x = body_mass_g)) + 
+      geom_histogram(binwidth = 20)
+
+ggplot(penguins, aes(x = body_mass_g)) +
+      geom_density()
+
+ggplot(penguins, aes(y = species, color = species)) +
+      geom_bar()
+
+ggplot(penguins, aes(x = species)) +
+      geom_bar(color ="red")
+
+ggplot(penguins, aes(x = species)) +
+      geom_bar(fill ="red",)
+
+ggplot(penguins, aes(x = species, y = body_mass_g)) +
+      geom_boxplot()
+
+ggplot(penguins, aes(x = body_mass_g, color = species)) +
+      geom_density(linewidth = 0.75)
+
+ggplot(penguins, aes(x = body_mass_g, color = species, fill = species)) +
+      geom_density(alpha = 0.5)
+
+ggplot(penguins, aes(x= island, fill = species)) +
+      geom_bar(position = "fill")
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+      geom_point()
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+      geom_point(aes(color = species, shape = island))
+
+ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+      geom_point(aes(color = species, shape = species)) +
+      facet_wrap(~island)
+                 
